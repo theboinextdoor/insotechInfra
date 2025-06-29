84a8a269657747d96539b94bd2d7ec11bc74a643
theboynextdoor <cman55245@gmail.com>
2025-06-29 17:56:07 +0530

    Added the Header and the footer Page to the Layout

---
+++ src/components/common/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="container flex items-center justify-between py-4 md:py-6 lg:py-8 mx-auto px-2 md:px-4 lg:px-6">
+      This is the Header File
+    </div>
+  );
+};
+
+export default Header;
